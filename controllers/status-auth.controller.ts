00ec15bb443e9b463ce38d7453d30a142217661b
Daniel <dleal.pizarro97@gmail.com>
2023-01-25 23:44:51 +0100

    initial commit

--- controllers/status-auth.controller.ts
+++
@@ -1,9 +0,0 @@
-import { Router, Request, Response } from 'express';
-
-const router = Router();
-
-router.get('/', (_req: Request, res: Response) => {
-    res.send({ result: 'Hello world!' });
-});
-
-export const StatusAuthController = router;
--- index.ts
+++ index.ts
@@ -1,19 +1,13 @@
 import express from 'express';
 import dotenv from 'dotenv';
-import { BasicAuthController } from './controllers/basic-auth.controller';
-import { StatusAuthController } from './controllers/status-auth.controller';
-import { BearerAuthController } from './controllers/bearer-auth.controller';
-import { LoginController } from './controllers/login.controller';
+import { QuestionsController } from './controllers/questions.controller';
 
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
-app.use('/', StatusAuthController);
-app.use('/basic', BasicAuthController);
-app.use('/bearer', BearerAuthController);
-app.use('/login', LoginController);
+app.use('/questions', QuestionsController);
 
 app.listen(port, () => {
     console.log(`⚡️[Server]: Server is running at https://localhost:${port}`);
