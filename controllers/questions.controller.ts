af4dd8cc3b2ee797d4b54bcf9bf2cb2ca27f3fe2
Daniel <dleal.pizarro97@gmail.com>
2023-02-12 16:00:59 +0100

    feat: disabling  CORS headers

--- controllers/questions.controller.ts
+++ controllers/questions.controller.ts
@@ -4,6 +4,7 @@ import { get_all_questions } from '../db/db';
 const router = Router();
 
 router.get('/', async (_req: Request, res: Response) => {
+    res.set('Access-Control-Allow-Origin', '*')
     const result = await get_all_questions();
 
     res.send(result);
