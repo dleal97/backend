2765fb4efc001e48dc696f96b0418e22ee64dc1a
Daniel <dleal.pizarro97@gmail.com>
2023-04-14 21:32:13 +0200

    refactor: use question use case instead of function

--- controllers/questions.controller.ts
+++ controllers/questions.controller.ts
@@ -1,13 +1,16 @@
 import { Router, Request, Response } from 'express';
-import { get_all_questions, check_newbd } from '../db/db';
+import { check_newbd } from '../db/db';
+import { SqlQuestionRepository } from '../infraestructure/question/sql-question-repository';
+import { GetAllQuestions } from '../use-cases/get-all-questions';
 
 const router = Router();
 
 router.get('/', async (_req: Request, res: Response) => {
     res.set('Access-Control-Allow-Origin', '*')
-    const result = await get_all_questions();
+    const useCase  = new GetAllQuestions( new SqlQuestionRepository());
+    const questions = await useCase.execute();
 
-    res.send(result);
+    res.send(questions);
 });
 
 router.get('/health', async (_req: Request, res: Response) => {
